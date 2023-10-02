c24bdca1a24ad6ba45254003f76fa517b11342c2
Ryan DCunha <rtd22@duke.edu>
2023-10-01 21:25:33 -0400

    fix load.sql

--- db/load.sql
+++ db/load.sql
@@ -14,3 +14,38 @@ SELECT pg_catalog.setval('public.products_id_seq',
 SELECT pg_catalog.setval('public.purchases_id_seq',
                          (SELECT MAX(id)+1 FROM Purchases),
                          false);
+
+\COPY Purchases FROM 'Seller.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
+\COPY Purchases FROM 'ProductsForSale.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
+\COPY Purchases FROM 'OrdersInProgress.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
+\COPY Purchases FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
+\COPY Purchases FROM 'LineItem.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
+\COPY Purchases FROM 'ProductReviews.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
+\COPY Purchases FROM 'SellerReviews.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
