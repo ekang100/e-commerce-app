cf3c3afdbf62cbdcb241c0deb41d30277ebb0ac5
Ellie_Kang <ek199@duke.edu>
2023-10-03 22:53:32 -0400

    product guru endpoint

--- app/models/product.py
+++ app/models/product.py
@@ -2,16 +2,19 @@ from flask import current_app as app
 
 
 class Product:
-    def __init__(self, productid, name, price, available):
-        self.produdctid = productid
+    def __init__(self, productid, name, price, category, available, avg_rating, seller_id):
+        self.productid = productid
         self.name = name
         self.price = price
+        self.category = category
         self.available = available
+        self.avg_rating = avg_rating
+        self.seller_id = seller_id
 
     @staticmethod
     def get(productid):
         rows = app.db.execute('''
-SELECT productid, name, price, available
+SELECT productid, name, price, category, available, avg_rating, seller_id
 FROM Products
 WHERE productid = :productid
 ''',
@@ -21,7 +24,7 @@ WHERE productid = :productid
     @staticmethod
     def get_all(available=True):
         rows = app.db.execute('''
-SELECT productid, name, price, available
+SELECT productid, name, price, category, available, avg_rating, seller_id
 FROM Products
 WHERE available = :available
 ''',
---
+++ app/products.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify, Blueprint, render_template
+bp = Blueprint('products', __name__)
+
+from .models.product import Product
+
+@bp.route('/', methods=['GET', 'POST'])
+def top_products():
+    if request.method == 'POST':
+        try:
+            k = int(request.form['k'])
+            products = Product.get_all(True)
+            sorted_products = sorted(products, key=lambda x: x.price, reverse=True)[:k]
+            return render_template('search.html', top_k_products=sorted_products)
+        except ValueError:
+            return "Invalid input. Please enter a valid number for K."
+    return render_template('index.html')
