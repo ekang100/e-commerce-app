e68702bd5829b79123f0afb2fc1ec662a41df071
tony cui <ac727@duke.edu>
2023-10-05 00:17:17 -0400

    fixed cart.py to use current_user

--- app/cart.py
+++ app/cart.py
@@ -15,18 +15,18 @@ bp = Blueprint('cart', __name__)
 @bp.route('/cart')
 def cart():
     #currentUID = current_user.id
-    #if current_user.is_authenticated:
+    if current_user.is_authenticated:
         #totalItemCount = Cart.get_unique_item_count(current_user.id)  #this will have to use cart and lineitem
         # return render_template('wishlist.html',
         #                        wishlist=wishlist)
-        allItemsInCart = LineItem.get_all_by_cartid_not_fulfilled(0,False)
-        updateCartFirst = Cart.update_total_cart_price(0) #replace 0's with current_user.id
-        singleCart = Cart.get_cartID_from_buyerid(0)
+        allItemsInCart = LineItem.get_all_by_cartid_not_fulfilled(Cart.get_cartID_from_buyerid(current_user.id),False)
+        updateCartFirst = Cart.update_total_cart_price(Cart.get_cartID_from_buyerid(current_user.id)) #replace 0's with current_user.id
+        singleCart = Cart.get_cartID_from_buyerid(current_user.id)
         return render_template('cart.html', singleCart = singleCart, ItemsInCart=allItemsInCart)
         #return redirect(url_for('cart.cart')) 
 
-    # else:
-    #     return jsonify({}), 404
+    else:
+         return jsonify({}), 404
 
     #return jsonify([item.__dict__ for item in wishlist])
 
