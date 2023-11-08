0aaa02888f98731780d649695add3abb4e06cf75
Bryant <bjc61@duke.edu>
2023-11-07 20:35:29 -0500

    Reverse Commit

--- app/models/lineitem.py
+++ app/models/lineitem.py
@@ -2,7 +2,7 @@ from flask import current_app as app
 
 
 class LineItem:
-    def __init__(self, lineid, cartid, productid, quantities, unitPrice, buyStatus, fulfilledStatus, time_purchased, time_fulfilled, orderid, sellerid):
+    def __init__(self, lineid, cartid, productid, quantities, unitPrice, buyStatus, fulfilledStatus, time_purchased, time_fulfilled, orderid):
         self.lineid = lineid
         self.cartid = cartid
         self.productid = productid
@@ -13,13 +13,12 @@ class LineItem:
         self.time_purchased = time_purchased
         self.time_fulfilled = time_fulfilled
         self.orderid = orderid
-        self.sellerid = sellerid
 
 
     @staticmethod
     def get(lineid):
         rows = app.db.execute('''
-SELECT *
+SELECT lineid, cartid, productid, quantities, unitPrice, buyStatus, time_purchased
 FROM LineItem
 WHERE lineid = :lineid
 ''',
@@ -53,7 +52,7 @@ WHERE productid = :productid
     @staticmethod
     def get_all_by_cartid_not_bought(cartid,buyStatus = False):
         rows = app.db.execute('''
-SELECT P.name, unitPrice, quantities,  LineItem.lineid, LineItem.orderid, LineItem.fulfilledStatus
+SELECT P.name, unitPrice, quantities,  LineItem.lineid
 FROM LineItem, Products P
 WHERE P.productid = LineItem.productid
 AND LineItem.cartid = :cartid
@@ -61,18 +60,17 @@ AND LineItem.buyStatus = False
 ''',
                               cartid=cartid)
         return [{"name": row[0], "price": row[1], "quantities": row[2], "lineid":row[3]} for row in rows]
-
+    
     @staticmethod
     def get_all_by_cartid_bought(cartid,buyStatus = True):
         rows = app.db.execute('''
-SELECT P.name, unitPrice, quantities,  LineItem.lineid, LineItem.orderid, fulfilledStatus
+SELECT P.name, unitPrice, quantities,  LineItem.lineid
 FROM LineItem, Products P
 WHERE P.productid = LineItem.productid
 AND LineItem.cartid = :cartid
-AND LineItem.buyStatus = :buyStatus
 ''',
-                              cartid=cartid, buyStatus = buyStatus)
-        return [{"name": row[0], "price": row[1], "quantities": row[2], "lineid":row[3], "orderid":row[4], "fulfilledStatus":row[5]} for row in rows]
+                              cartid=cartid)
+        return [{"name": row[0], "price": row[1], "quantities": row[2], "lineid":row[3]} for row in rows]
     
 
 
@@ -97,14 +95,3 @@ WHERE lineid = :lineid
             lineid = lineid)
         return None
 
-
-#this is new
-    @staticmethod
-    def get_all_lineitems_by_orderid(orderid):
-                rows = app.db.execute('''
-SELECT *
-FROM LineItem
-WHERE LineItem.orderid := orderid
-''',
-            orderid=orderid) 
-                return LineItem(*(rows[0])) if rows else None
