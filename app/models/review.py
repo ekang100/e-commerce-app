d6786a0b8a1e7b9afeb2a4e1c43eeb939934daef
davidyao <dy95@duke.edu>
2023-11-09 15:09:42 -0500

    "older schema changes"

--- app/models/review.py
+++ app/models/review.py
@@ -1,5 +1,4 @@
-
-
+from datetime import datetime
 from flask import current_app as app
 
 class Reviews:
@@ -13,25 +12,61 @@ class Reviews:
 
     @staticmethod
     def get(type, entity_id, uid):
-        rows = app.db.execute('''
+        try:
+            rows = app.db.execute('''
 SELECT type, entity_id, uid, rating, comments, date
 FROM Reviews
 WHERE type = :type AND entity_id = :entity_id AND uid = :uid
 ''',
-                              type=type,
-                              entity_id=entity_id,
-                              uid=uid)
-        return Reviews(*(rows[0])) if rows else None
-
+                                  type=type,
+                                  entity_id=entity_id,
+                                  uid=uid)
+            return Reviews(*(rows[0])) if rows else None
+        except Exception as e:
+            # You can log the error here for further debugging
+            raise ValueError(f"Error fetching review: {str(e)}") from e
+        
     @staticmethod
     def get_most_recent_by_uid(uid, limit=5):
-        rows = app.db.execute('''
-SELECT type, entity_id, uid, rating, comments, date
-FROM Reviews
-WHERE uid = :uid
-ORDER BY date DESC
-LIMIT :limit
-''',
-                              uid=uid,
-                              limit=limit)
-        return [Reviews(*row) for row in rows]
+            rows = app.db.execute('''
+    SELECT type, entity_id, uid, rating, comments, date
+    FROM Reviews
+    WHERE uid = :uid
+    ORDER BY date DESC
+    LIMIT :limit
+    ''',
+                                uid=uid,
+                                limit=limit)
+            if not rows:
+                raise ValueError(f"No reviews found for user_id: {uid}")
+            return [Reviews(*row) for row in rows]
+    
+    @staticmethod
+    def post_review(type, entity_id, uid, rating, comments):
+        try:
+            # Check if the review already exists for the user
+            existing_review = Reviews.get(type, entity_id, uid)
+            if existing_review:
+                return "You have already reviewed this item."
+
+            # Construct the SQL query based on the review type
+            params = {
+                'product_id': entity_id if type == 'product' else None,
+                'seller_id': entity_id if type == 'seller' else None,
+                'uid': uid,
+                'type': type,
+                'rating': rating,
+                'comments': comments,
+                'date': datetime.now()
+            }
+
+            query = '''
+INSERT INTO Reviews (product_id, seller_id, uid, type, rating, comments, date)
+VALUES (:product_id, :seller_id, :uid, :type, :rating, :comments, :date)
+'''
+
+            app.db.execute(query, **params)
+
+            return "Review posted successfully."
+        except Exception as e:
+            raise ValueError(f"Error posting review: {str(e)}") from e
