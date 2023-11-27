d226dbf13bb9cdc4324c18cbfacf945106594c48
tony cui <ac727@duke.edu>
2023-11-27 15:46:02 -0500

    i forgor to add fulfillment time but its added now

--- app/models/lineitem.py
+++ app/models/lineitem.py
@@ -105,7 +105,7 @@ ORDER BY P.name
     @staticmethod
     def get_all_by_cartid_bought(cartid,buyStatus = True):
         rows = app.db.execute('''
-SELECT P.name, unitPrice, quantities,  LineItem.lineid, LineItem.orderid, fulfilledStatus, time_purchased, U.firstname, U.lastname
+SELECT P.name, unitPrice, quantities,  LineItem.lineid, LineItem.orderid, fulfilledStatus, time_purchased, U.firstname, U.lastname, LineItem.time_fulfilled
 FROM LineItem, Products P, Users U
 WHERE P.productid = LineItem.productid
 AND LineItem.cartid = :cartid
@@ -115,7 +115,7 @@ AND U.id = LineItem.sellerid
 ORDER BY orderid DESC
 ''',
                               cartid=cartid, buyStatus = buyStatus)
-        return [{"name": row[0], "price": row[1], "quantities": row[2], "lineid":row[3], "orderid":row[4], "fulfilledStatus":row[5], "time_purchased":row[6], "firstname":row[7], "lastname":row[8]} for row in rows]
+        return [{"name": row[0], "price": row[1], "quantities": row[2], "lineid":row[3], "orderid":row[4], "fulfilledStatus":row[5], "time_purchased":row[6], "firstname":row[7], "lastname":row[8], "time_fulfilled":row[9]} for row in rows]
     
     # make a new line item or update if it already exists when adding to cart
     @staticmethod
