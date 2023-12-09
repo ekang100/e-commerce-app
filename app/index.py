c07b08fa49b3ba806f3492f0bee087846662da80
davidyao <dy95@duke.edu>
2023-12-09 10:50:26 -0500

    "resolves conflicts"

--- app/index.py
+++ app/index.py
@@ -14,20 +14,49 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
-    categories = Product.get_categories()
-    clean_text = [re.sub(r"\('([^']+)',\)", r"\1", text) for text in categories]
-    # get all available products for sale:
+    # get page for pagination
     page = int(request.args.get('page', 1))
-    per_page = 10 # can make this adjustable in the future
-    all_products = Product.get_all()
+    per_page = 10 # can make this adjustable in the future, displays given amount of products on a page
+
+    # get sorting preferences
     sort_by = request.args.get('sort_by', default='None')
-    products = Product.get_paginated(True, page, per_page, sort_by)
-    max_page = int(math.ceil(len(all_products) / per_page))
-    categories = Product.get_categories()
-    clean_text = [re.sub(r"\('([^']+)',\)", r"\1", text) for text in categories]
-    # render the page by adding information to the index.html file
-    return render_template('index.html',
-                           avail_products=products, page=page, max_page=max_page, categories=clean_text)
+
+    # check sorting preferences to prohibit vulnerability and assign new variable to something more easily passed to a query
+    sort_by_column = sort_assignment(sort_by)
+    
+    # get ratings preferences
+    rating = request.args.get('rating', default=0)
+    rate = int(rating)
+
+    # get availability status preferences
+    if request.method == 'POST':
+        # check if the checkbox was submitted and update in_stock variable
+        if 'in_stock' in request.form:
+            in_stock = True
+        else:
+            in_stock = False
+    else:
+        in_stock = request.args.get('in_stock')
+
+    # if the user only wants to see available products...
+    if in_stock:
+        available = True
+        products = Product.get_paginated_avail(sort_by_column, page, rate, available) # only returns available products
+        total = int(Product.get_num_products_avail(rate, available))
+    else:
+        total = int(Product.get_num_products(rate))
+        products = Product.get_paginated(sort_by_column, page, rate) # does not take into account availability status
+    
+    max_page = int(math.ceil(total / per_page)) # calculate the last page of pagination
+    categories = Product.get_categories() # get a list of categories to display in the dropdown
+    clean_text = [re.sub(r"\('([^']+)',\)", r"\1", text) for text in categories] # remove formatting from category list
+    if current_user.is_authenticated:
+        buy_again = Product.get_purchases_by_uid(current_user.id)
+        product_ids = [product.get("productid") for product in buy_again]
+        return render_template('index.html',
+                           product_ids=product_ids, rating=rating, avail_products=products, per_page=per_page, page=page, max_page=max_page, categories=clean_text, total=total, sort_by=sort_by, in_stock=in_stock)
+
+
 
 @bp.route('/account')
 def index2():
